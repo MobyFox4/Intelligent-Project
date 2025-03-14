a0d3dfccb51acdb3265bc4db2620d1b3e8d85e0e
Moby <mostmostmickey9828@gmail.com>
2025-03-14 23:56:40 +0700

    Demo add

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -5,6 +5,11 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 import gdown
+from PIL import Image
+
+url = "https://drive.google.com/uc?id=1f7N6eOyBp8FTwmhx299Trz7dJNLoXMaH"
+output = "Dog_Cat_ImagePredict_model.keras"
+gdown.download(url, output, quiet=False)
 
 # Code ML
 MLcode1 = "import pandas as pd\ndf = pd.read_csv('../Dataset/mushroom_overload.csv')\ndf"
@@ -416,7 +421,36 @@ elif page == "Machine Learning Demo":
 
     
 elif page == "Neural Network Model":
+
+    def load_model():
+        model = tf.keras.models.load_model("Dog_Cat_ImagePredict_model.keras")
+        return model
+
+    model = load_model()
+
+    def preprocess_image(image):
+        image = image.resize((150, 150))  # ปรับขนาดให้ตรงกับโมเดล
+        image = np.array(image) / 255.0  # ทำให้เป็นค่า [0,1]
+        image = np.expand_dims(image, axis=0)  # เพิ่มมิติให้เป็น (1, 150, 150, 3)
+        return image
+    
     st.title("🤖 Neural Network Demo 🤖")
-    st.write("อัปโหลดไฟล์รูปภาพเพื่อเช็คว่าเป็น หมา หรือ แมว")
+    st.write("อัปโหลดไฟล์รูปภาพเพื่อเช็คว่าเป็น 🐶หมา หรือ 🐱แมว")
+
+# อัปโหลดรูปภาพ
+uploaded_file = st.file_uploader("เลือกไฟล์รูปภาพ", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="รูปที่อัปโหลด", use_column_width=True)
+
+    # ปุ่ม Predict
+    if st.button("🔍 Predict"):
+        processed_image = preprocess_image(image)
+        prediction = model.predict(processed_image)
+        result = "🐶 หมา" if prediction[0][0] > 0.5 else "🐱 แมว"
+
+        st.subheader("🔎 ผลลัพธ์การทำนาย:")
+        st.success(result)
 
 
